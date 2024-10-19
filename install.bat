bc211b8a6e1aa03e764702b7e965853ca9bdaee8
corporate9601 <64847353+corporate9601@users.noreply.github.com>
2024-10-19 17:08:09 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
+ECHO Installed Python dependencies, installing FFMpeg
 winget install "FFmpeg (Essentials Build)"
 ECHO Installation done. Click any key to close window
 pause
